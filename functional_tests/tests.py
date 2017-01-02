1e6c9d25bd22b43bb53a896efa2e73a5760588da
simpen <luoxr258@sina.com>
2017-01-02 17:18:03 +0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,13 @@
 import unittest
 import time
+
+from django.test import LiveServerTestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # 她注意到网页的标题和头部都包含“To-Do”这个单词
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -63,7 +66,3 @@ class NewVisitorTest(unittest.TestCase):
         # 她访问那个 URL，发现她的待办事项列表还在
 
         # 她很满意，去睡觉了
-
-
-if __name__ == '__main__':
-    unittest.main()
