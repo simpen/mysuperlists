2ac2cc5411e36f24534355713b5c63b82453217a
simpen <luoxr258@sina.com>
2017-01-02 12:12:07 +0800

    FT test 2 lists item

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,7 @@
+import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -33,21 +34,26 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了"1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # 页面中又显示了一个文本框，可以输入其它的待办事项
         # 她输入了 "Use peacock feathers to make a fly"（使用孔雀羽毛做假蝇）
         # 伊迪丝做事很有条理
-        self.fail(msg='Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
         # 页面再次更新，她的清单是显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
 
         # 伊迪丝想知道这个网站是否会记住她的清单
-
+        self.fail(msg='Finish the test!')
         # 她看到网站为她生成了一个唯一的网址
 
         # 而且页面中有一些文字解说这个功能
@@ -55,8 +61,6 @@ class NewVisitorTest(unittest.TestCase):
         # 她访问那个 URL，发现她的待办事项列表还在
 
         # 她很满意，去睡觉了
-
-        browser.quit()
 
 
 if __name__ == '__main__':
