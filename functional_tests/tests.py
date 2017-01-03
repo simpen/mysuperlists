11c949fe6589fcd32a4e4a6f79af68008ffed97d
simpen <luoxr258@sina.com>
2017-01-03 23:58:11 +0800

    refactor templates to use a base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -106,10 +106,12 @@ class NewVisitorTest(LiveServerTestCase):
     def test_layout_and_styling(self):
         # 伊迪丝访问首页
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
+        # self.browser.set_window_size(1024, 768)
         print(self.browser.get_window_size(), ...)
         # 她看到输入框完美地居中显示
         inputbox = self.browser.find_element_by_id('id_new_item')
+        # time.sleep(100)
+        # print(inputbox.size, inputbox.location)
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
 
